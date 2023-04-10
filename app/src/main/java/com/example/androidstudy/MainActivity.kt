874b37c9097cc14dd31d7a20120f886c5a384b0c
DESKTOP-NA5E2NJ\82106 <valent9@naver.com>
2023-04-10 21:18:11 +0900

    [김서현]  - 1주차 과제 : 코틀린 주요 문법 사용해보기

--- app/src/main/java/com/example/androidstudy/MainActivity.kt
+++ app/src/main/java/com/example/androidstudy/MainActivity.kt
@@ -3,9 +3,33 @@ package com.example.androidstudy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+// 유저 환영 문구
+fun userData(userName: String, userGrade: Int, userMajor: String): Unit{
+
+    if(userGrade == 1){
+        println("$userMajor 학과 신입생 $userName 님, 안녕하세요!")
+    } else{
+        println("$userMajor 학과 $userGrade 학년 $userName 님, 안녕하세요!")
+    }
+
+}
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 앱 테스트 문구
+        val welcomeCode = "Hello World!!"
+        println(welcomeCode)
+
+        val userMajor: String = "정보통신"
+        val userGrade : Int = 3
+        val userName : String = "김서현"
+
+        userData(userMajor, userGrade, userName)
+
     }
 }
